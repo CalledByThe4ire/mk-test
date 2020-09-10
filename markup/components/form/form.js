ae5c679c76c5c628011443dacdc58387f1356596
Andrey Karamnoff <karamnoff@gmail.com>
2020-09-10 09:30:47 +0300

    add form' validation mapping for email & passwordConfirmation

--- markup/components/form/form.js
+++ markup/components/form/form.js
@@ -7,21 +7,76 @@ document.addEventListener(`DOMContentLoaded`, () => {
     submit: document.querySelector('form [type="submit"]')
   };
 
+  const validationMapping = {
+    email: {
+      isNotEmpty(value) {
+        return value.length !== 0;
+      },
+      isValid(value) {
+        return /\S+@\S+\.\S+/.test(value.toLowerCase());
+      }
+    },
+    passwordConfirmation: {
+      isNotEmpty(value) {
+        return value.length !== 0;
+      },
+      isValid(value) {
+        const password = formElements.password.value;
+
+        return value === password;
+      }
+    }
+  };
+
+  const isFieldValid = (fieldName, fieldValue) =>
+    Object.keys(validationMapping[fieldName]).every((key) => validationMapping[fieldName][key](fieldValue));
+
   [formElements.email, formElements.passwordConfirmation].forEach((element) =>
     element.addEventListener('blur', ({ target }) => {
+      const formFieldElement = target.parentElement;
+
+      const feedbackElement = formFieldElement.lastElementChild;
+
+      feedbackElement.classList.add('form-field__feedback');
+
       switch (target.name) {
         case 'email':
-          target.parentElement.classList.toggle('form-field--invalid', target.value === '');
+          formFieldElement.classList.toggle('form-field--invalid', !isFieldValid(target.name, target.value));
+
+          if (!isFieldValid(target.name, target.value)) {
+            if (!validationMapping[target.name].isNotEmpty(target.value)) {
+              feedbackElement.textContent = 'Поле является обязательным для заполнения';
+            } else {
+              feedbackElement.textContent = 'Указанный e-mail является некорректным';
+            }
+
+            formFieldElement.append(feedbackElement);
+          }
+
           break;
+
         case 'passwordConfirmation':
-          target.parentElement.classList.toggle(
-            'form-field--invalid',
-            target.value !== formElements.password.value
-          );
+          formFieldElement.classList.toggle('form-field--invalid', !isFieldValid(target.name, target.value));
+
+          if (!validationMapping[target.name].isValid(target.value)) {
+            feedbackElement.textContent = 'Введённые пароли не совпадают';
+          }
+
           break;
         default:
           throw new Error(`Unknown name: ${target.name}`);
       }
+    })
+  );
+
+  [formElements.email, formElements.passwordConfirmation].forEach((element) =>
+    element.addEventListener('focus', ({ target }) => {
+      const formFieldElement = target.parentElement;
+
+      const feedbackElement = formFieldElement.lastElementChild;
+
+      formFieldElement.classList.remove('form-field--invalid');
+      feedbackElement.textContent = '';
     })
   );
 });
